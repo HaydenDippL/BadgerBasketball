ecee3d5a32d8eea08ed0e49e181370cff60ea314
Hayden Dippel <hdippel@wisc.edu>
2024-10-07 10:28:01 -0500

    changed fetch to netlify domain

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -63,8 +63,9 @@ function Schedule(props) {
 
         // console.log('http://localhost:3999/data?' + params)
 
-        fetch('https://www.uwopenrecrosterbackend.xyz/data?' + params, { signal })
+        // fetch('https://www.uwopenrecrosterbackend.xyz/data?' + params, { signal }) // legacy url
         // fetch('http://localhost:3999/data?' + params, { signal }) // used for local testing
+        fetch("./.netlify/functions/schedule")
             .then(res => res.json())
             .then(data => {
                 // if this is the current schedule, display it
