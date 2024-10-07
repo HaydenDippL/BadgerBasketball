287723afeca5a44218de9daa5d215e80a358f7ab
Hayden Dippel <hdippel@wisc.edu>
2024-10-07 15:09:31 -0500

    create connection instead of pool to use less resources

--- Frontend/netlify/helpers/logging.js
+++ Frontend/netlify/helpers/logging.js
@@ -1,39 +1,32 @@
-import { createPool, format } from "mysql"
+import { createConnection, format } from "mysql"
 // import dotenv from "dotenv"
 import * as fs from "fs"
 
 // dotenv.config()
 
-const pool = createPool({
-    host: process.env.TIDB_HOST || '127.0.0.1',
-    port: process.env.TIDB_PORT || 4000,
-    user: process.env.TIDB_USER || 'root',
-    password: process.env.TIDB_PASSWORD || '',
-    database: process.env.TIDB_DATABASE || 'test',
-    ssl: process.env.TIDB_ENABLE_SSL === 'true' ? {
-        minVersion: 'TLSv1.2',
-        ca: process.env.TIDB_CA_PATH ? fs.readFileSync(process.env.TIDB_CA_PATH) : undefined
-    } : null,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 20
-});
+function createDBConnection() {
+    return createConnection({
+        host: process.env.TIDB_HOST || '127.0.0.1',
+        port: process.env.TIDB_PORT || 4000,
+        user: process.env.TIDB_USER || 'root',
+        password: process.env.TIDB_PASSWORD || '',
+        database: process.env.TIDB_DATABASE || 'test',
+        ssl: process.env.TIDB_ENABLE_SSL === 'true' ? {
+            minVersion: 'TLSv1.2',
+            ca: process.env.TIDB_CA_PATH ? fs.readFileSync(process.env.TIDB_CA_PATH) : undefined
+        } : null
+    });
+}
 
 // async function that allows you call vall the logging DB
 export async function query(query, values) {
     return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            const sql = format(query, values)
-            connection.query(sql, (err, data) => {
-                connection.destroy();
-                return err ? reject(err) : resolve(data);
-            })
-        })
+        const conn = createDBConnection();
+        const sql = format(query, values);
+        conn.query(sql, (err, data) => {
+            conn.end();
+            return err ? reject(err) : resolve(data);
+        });
     })
 }
 
@@ -46,7 +39,7 @@ export async function logging(date_queried, gym, gym_facility, session_id, IP, d
             (?, ?, 1, ?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
             num_queries = num_queries + 1`;
-    await query(update_insert_session_sql, [session_id, IP, date_of_query, time_of_query, device, browser])
+    await query(update_insert_session_sql, [session_id, IP, date_of_query, time_of_query, device, browser]);
 
     const insert_queries_sql = 'INSERT INTO queries (date_queried, gym, gym_facility, session_id) VALUES (?, ?, ?, ?);';
     await query(insert_queries_sql, [date_queried, gym, gym_facility, session_id ?? null]);
