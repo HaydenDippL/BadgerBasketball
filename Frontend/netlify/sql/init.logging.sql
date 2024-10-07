de42328e0b6d2677286fbb412d30b8fd095311d5
Hayden Dippel <hdippel@wisc.edu>
2024-10-07 12:18:00 -0500

    sql files added

---
+++ Frontend/netlify/sql/init.logging.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sessions(
+    session_id VARCHAR(36) PRIMARY KEY,
+    IP TEXT,
+    num_queries INT,
+    date_of_queries DATE,
+    time_of_queries TIME,
+    device VARCHAR(40),
+    browser VARCHAR(40)
+);
+
+CREATE TABLE queries(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    date_queried DATE,
+    gym VARCHAR(10),
+    gym_facility VARCHAR(20),
+    session_id VARCHAR(36),
+    FOREIGN KEY(session_id) REFERENCES sessions(session_id)
+);
